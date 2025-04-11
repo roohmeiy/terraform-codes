3287ce26adad9115d90fa5d4e6400e591b43f5ed
pihu <its.official.pihu@gmail.com>
2025-04-11 19:04:19 +0530

    eks

---
+++ eks/variables.tf
@@ -0,0 +1,13 @@
+locals {
+
+  region          = "us-west-1"
+  name            = "my-eks-cluster"
+  vpc_cidr        = "10.0.0.0/16"
+  azs             = ["us-east-2a", "us-east-2b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+  intra_subnets   = ["10.0.5.0/24", "10.0.6.0/24"]
+  tags = {
+    example = local.name
+  }
+}
